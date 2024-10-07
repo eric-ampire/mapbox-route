ee40382aab10df7597549078631a740a0ccf87d1
Eric Ampire <eric.ampire@brp.com>
2024-10-07 02:42:27 -0400

    Initial Commit

---
+++ mapbox-navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mapbox-navigation
+//
+//  Created by Eric Ampire (Admin) on 2024-10-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mapbox-navigation/mapbox_navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapbox_navigationApp.swift
+//  mapbox-navigation
+//
+//  Created by Eric Ampire (Admin) on 2024-10-07.
+//
+
+import SwiftUI
+
+@main
+struct mapbox_navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
